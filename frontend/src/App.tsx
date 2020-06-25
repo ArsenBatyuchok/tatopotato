173d991eadf1892a7202de3d027e70a67c1f73ca
Arsen Batyuchok <arsen.b@matic.com>
2020-06-25 22:10:02 +0300

    added data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import data from './data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          zdarova
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.dishes.map((d, i) => (
+        <div key={i} className="dish-item">
+          <div>{d.name}</div>
+          <div>{d.price}</div>
+        </div>
+      ))}
     </div>
   );
 }
